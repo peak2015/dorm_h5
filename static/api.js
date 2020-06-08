fd15923fd878bc6b22cf965fafe13a62646ddda6
朱祥龙 <740152072@qq.com>
2020-06-08 11:13:13 +0800

    ee

--- static/api.js
+++ static/api.js
@@ -1,7 +1,7 @@
 (function () {
 	window.configs = {
-		api: 'http://106.12.3.212:8002',
-		//api: 'http://localhost:8001',
+		//api: 'http://106.12.3.212:8002',
+		api: 'http://192.168.0.103:8001',
 		school: '安徽工程大学',
 		system: '国际工程师学院宿舍管理及分析系统',
 		title: '智慧校园 智慧后勤 智慧宿舍',
