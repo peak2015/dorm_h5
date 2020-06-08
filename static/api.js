d684c22bac7573dbf5cb44463c6cc9fb537b251f
朱祥龙 <740152072@qq.com>
2020-06-08 11:07:44 +0800

    qq

---
+++ static/api.js
@@ -0,0 +1,10 @@
+(function () {
+	window.configs = {
+		api: 'http://106.12.3.212:8002',
+		//api: 'http://localhost:8001',
+		school: '安徽工程大学',
+		system: '国际工程师学院宿舍管理及分析系统',
+		title: '智慧校园 智慧后勤 智慧宿舍',
+		headertitle: '人脸识别宿舍管理 - '
+	};
+})();
